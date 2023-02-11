ec2bc432542a1869db20805b0c42c78eff19be44
onicolli <onicolli>
2023-02-11 23:26:41 +0600

    Do HomeWork_02

---
+++ Task_02/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+Console.Write($"{num}");
+
+void Count(int sum = 0)
+{
+    while (num > 0)
+    {
+        sum = sum + num % 10;
+        num = num /10 ;
+    }
+    Console.Write($" -> {sum}");
+}
+Count(0);
+
+            
